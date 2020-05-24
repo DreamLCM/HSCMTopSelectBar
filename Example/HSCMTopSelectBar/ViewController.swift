202e588b60f00fc3a749520dcf266d85ee1a286c
Cimons <953330953@qq.com>
2020-05-24 14:11:59 +0800

    Initial commit

---
+++ Example/HSCMTopSelectBar/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  HSCMTopSelectBar
+//
+//  Created by 953330953@qq.com on 05/24/2020.
+//  Copyright (c) 2020 953330953@qq.com. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
+
